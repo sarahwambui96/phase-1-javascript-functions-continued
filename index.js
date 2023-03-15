9d8e642ada86bf62218385a9b94fe6d008c5babe
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-15 13:38:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+//  defines saturdayFun function
+function saturdayFun( activity ="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+// defines mondayWork function
+function mondayWork( activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork()
+mondayWork("work from home")
+
+//defines wrapAdjective function
+let wrapAdjective = function(style="*") {
+    return function(word="special") {
+      return `You are ${style}${word}${style}!`
+    }
+  }
+  wrapAdjective("*") ("a hard worker")
+  wrapAdjective("||") ("a dedicated programmer")
